3db81aea83de9d067a5b6150d24949a9e1d587e3
Reyad <mdreyadh749@gmail.com>
2023-05-12 14:49:36 +0600

    3rd commit

--- Riyadh/myApp/views.py
+++ Riyadh/myApp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 # Create your views here.
 def home(request):
-   return render(request,"myApp/home.html")
+   return render(request, "myApp/home.html")
 def signup(request):
-    return render(request,"myApp/signup.html")
+    return render(request, "myApp/signup.html")
 
 def signin(request):
-    return render(request,"myApp/signin.html")
+    return render(request, "myApp/signin.html")
 
 def signout(request):
     pass
