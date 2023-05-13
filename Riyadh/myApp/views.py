5eb26971e929159189916dd0fe61dd593f1d409f
Reyad <mdreyadh749@gmail.com>
2023-05-13 21:10:11 +0600

    4th commit

--- Riyadh/myApp/views.py
+++ Riyadh/myApp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from collections import UserDict
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 
@@ -7,6 +10,27 @@ from django.http import HttpResponse
 def home(request):
    return render(request, "myApp/home.html")
 def signup(request):
+
+    if request.method == "post":
+        username = request.post['username']
+        firstname = request.post['first name']
+        lastname = request.post['last name']
+        email = request.post['email']
+        password= request.post['password']
+        Cpassword = request.post['Cpassword']
+
+        myuser = User.object.create_user(username, email, password)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
+
+        myuser.save()
+
+        messages.success(request, "Your Account has been successfully created. ")
+
+        return redirect("signin")
+
+
+
     return render(request, "myApp/signup.html")
 
 def signin(request):
